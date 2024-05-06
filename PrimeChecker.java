c63c97b149a31a6cc312b84a256c0894584d49da
Darwin_A <168660469+darwina200404@users.noreply.github.com>
2024-05-06 22:04:40 +0530

    Add files via upload

---
+++ PrimeChecker.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class PrimeChecker {
+    public static boolean isPrime(int num) {
+        if (num < 2) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int number;
+        Scanner sc =new Scanner(System.in);
+        number=sc.nextInt();
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+    }
+}
